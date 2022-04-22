59d6da48e389504169c92f0a2e8070bcc3c90bd4
Jorge <jorjoluiso@gmail.com>
2022-04-21 19:38:01 -0500

    drop sequences

--- sql/sequences_trigger.sql
+++ sql/sequences_trigger.sql
@@ -1,3 +1,17 @@
+BEGIN
+    FOR cc IN (
+        SELECT
+            sequence_name
+        FROM
+            all_sequences
+        WHERE
+            sequence_owner = user
+    ) LOOP
+        EXECUTE IMMEDIATE 'DROP SEQUENCE ' || cc.sequence_name;
+    END LOOP;
+END;
+/
+
 CREATE SEQUENCE SEQ_TAXPAYERS INCREMENT BY 1 START WITH 1 MINVALUE 1 NOCACHE ORDER;
 
 CREATE OR REPLACE TRIGGER tr_seq_taxpayers
